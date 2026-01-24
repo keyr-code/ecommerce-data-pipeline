b26d44a2d01204ef8c93798b2829e89725dc326b
Kiren <keyrcode@gmail.com>
2026-01-24 16:04:42 +0200

    Removed full path

--- src/silver/fix_data.py
+++ src/silver/fix_data.py
@@ -4,4 +4,4 @@ df = pd.read_csv("/Users/kirenp/kiren2/projects_inprogress/2026_projects/Data En
 df["age"] = pd.to_numeric(df["age"], errors="coerce") # This converts column to float
 df["age"] = df["age"].fillna(0).astype(int)  # Replace NaN with 0, then convert to int
 
-#df.to_csv("/Users/kirenp/kiren2/projects_inprogress/2026_projects/Data Engineering Apprenticeship/projects/stage_1/data/raw_customers_cleaned.csv", index=False)
+#df.to_csv("raw_customers_cleaned.csv", index=False)
